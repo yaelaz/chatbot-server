7ce6f6d36fa21d0370416238bf8cdbaedb9169c8
Yael Azriel <yaela@Yaels-MacBook-Pro.local>
2019-04-04 15:55:35 +0300

    minor changes

--- server.js
+++ server.js
@@ -15,40 +15,43 @@ app.get('/', (req, res) => {
 });
 
 app.get('/welcome', (req, res) => {
-  let sessionId = req.cookies['chatbot_id'];
+  const sessionId = req.cookies['chatbot_id'];
   if(!sessionId || !sessionStorage[sessionId]){
-    console.log('no session id');
     sessionStorage.waitForName = true;
-    res.send([{text: `Hi, I'm Maya! Today you’re going to help me to ace my game`, type: 'bot'},
-    {text: `Let's start by telling me your name`, type: 'bot'}]);
+    send(res, [{text: `Hi, I'm Maya! Today you’re going to help me to ace my game`},
+    {text: `Let's start by telling me your name`}]);
   } else {
-    console.log('there is session id');
     const name = sessionStorage[sessionId].name;
-    res.send([{text: `Nice to see you again ${name}. Let's pick this up from where we left off`, type: 'bot'},
-    {text: `List any mathematical expression you can think of - I'll crunch it in no time`, type: 'bot'}]);
+    send(res, [{text: `Nice to see you again ${name}. Let's pick this up from where we left off`},
+    {text: `List any mathematical expression you can think of - I'll crunch it in no time`}]);
   }
 });
 
 app.get('/answer/:param', (req, res) => {
+  let param = req.params.param;
   if(sessionStorage.waitForName){
-      console.log('name is provided');
       sessionStorage.waitForName = false;
-      sessionId = index++;
-      sessionStorage[sessionId] = { name: req.params.param };
-      res.cookie('chatbot_id', sessionId, { maxAge: 900000, httpOnly: true });
-      res.send([{text: `Nice to meet you ${req.params.param}!`, type: 'bot'},
-      {text: `Alright, this is how it's going to work`, type: 'bot'},
-      {text: `List any mathematical expression you can think of - I'll crunch it in no time`, type: 'bot'}]);
+      const sessionId = index++;
+      param = param.split(' ')[0];
+      sessionStorage[sessionId] = { name: param };
+      res.cookie('chatbot_id', sessionId);
+      send(res, [{text: `Nice to meet you ${param}!`},
+      {text: `Alright, this is how it's going to work`},
+      {text: `List any mathematical expression you can think of - I'll crunch it in no time`}]);
   } else {
     try {
-      const num = math.eval(req.params.param);
-      if(num){
-        res.send([{text: num.toString(), type: 'bot'}]);
-      }
+      const num = math.eval(param.toLowerCase());
+      send(res, [{text: num.toString()}, {text: 'This was easy, give me something harder 🤓'}]);
     } catch(e) {
-      res.send([{text: `Sorry, can't help you with that`, type: 'bot'}]);
+      send(res, [{text: `Sorry, can't help you with that 🤷`}]);
     }
   }
 });
+
+function send(res, data) {
+  setTimeout(() => {
+    res.send(data);
+  }, 1000);
+}
 
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
